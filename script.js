d857aac2fdfbd79a0bb37878fc68565f69a42253
shrawankansi <shrawankansi@gmail.com>
2024-04-02 15:02:24 +0530

    updaye

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + ` &appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data);
+  
 
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°C";
